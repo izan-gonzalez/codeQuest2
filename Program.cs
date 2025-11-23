cd6067008482f2a15b92ada1800aab9ce32a72c7
izan <izan.gonzalez.7e8@itb.cat>
2025-11-23 23:56:41 +0100

    Chapter 3 done

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.ComponentModel.Design;
 using System.Diagnostics.Metrics;
 using System.Numerics;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+
 
 
 
@@ -12,6 +17,7 @@ public class Program
 {
     static void Main()
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
         const string MenuTitle = "===== MAIN MENU - CODEQUEST =====";
         const string MenuOption1 = "1. Train your wizard";
         const string MenuOption2 = "2. Check the dungeon";
@@ -26,21 +32,70 @@ public class Program
         // Level 1
         const string GuardAsk = "Guard: Whats your name Wizard?";
         const string GuardRage = "Guard: THAT NAME IS NOT ON THE LIST!, TELL ME YOUR REAL NAME!!";
-        const string EspecificationName = "The name must have 1 capital letter and rest in lower case";
         const string GuardLore = "Guard: Ho you are the wizard ho robbed one apple, you will be here for other 5 days";
+        const string WizardTitle = "Title: ";
         const string WizardTitle1 = "Raoden the Elantrian";
         const string WizardTitle2 = "Zyn the Buguejat";
         const string WizardTitle3 = "Arka Nullpointer";
         const string WizardTitle4 = "Elarion of the Embers";
         const string WizardTitle5 = "ITB-Wizard the Grey";
+        const string WizardText1 = ", You repeat in the 2nd call.";
+        const string WizardText2 = ", You still confuse the rod with a spoon.";
+        const string WizardText3 = ", You are a Summoner of Magic Breezes.";
+        const string WizardText4 = ", Wow! You can summon dragons without burning down the lab!";
+        const string WizardText5 = ", You have reached the rank of Master of Arcana!.";
         //string menuSubtitle = $"===== Welcome,{realName} the {} with level {} =====";
+        // Level 2
+        const string DiceOne = "  ________ \n /       /|\n/_______/ |\n|       | |\n|   o   | /\n|       |/\n'-------' ";
+        const string DiceTwo = "  ________ \n /       /|\n/_______/ |\n|o      | |\n|       | /\n|     o |/\n'-------' ";
+        const string DiceTree = "  ________ \n /       /|\n/_______/ |\n|o      | |\n|   o   | /\n|     o |/\n'-------' ";
+        const string DiceFour = "  ________ \n /       /|\n/_______/ |\n| o   o | |\n|       | /\n| o   o |/\n'-------' ";
+        const string DiceFive = "  ________ \n /       /|\n/_______/ |\n| o   o | |\n|   o   | /\n| o   o |/\n'-------' ";
+        const string DiceSix = "  ________ \n /       /|\n/_______/ |\n| o   o | |\n| o   o | /\n| o   o |/\n'-------' ";
+        const string EnemyEncounter = "You have and encounter with: ";
+        const string EnemyEncounterHp = ", the hp of the enemy are :";
+        const string ActionAttack = "Click a key to hit";
+        const string TotalHp = "total hp are: ";
+        const string LvlUp = " level up to: ";
+        // Level 3 
+        const string MineX = "Put a number between 0 to 4 to chose line to mine";
+        const string MineY = "Put a number between 0 to 4 to chose column to mine";
+
+
+
+
+
+
+
+
+        int totalBitcoins = 0;
+        int WizardLvl = 0;
+
+
+
+
 
 
         const string Error = "Input error try again";
         int oP = 0, powerWizard, totalWizard = 0;
 
 
+
+
+
+
+
+
         string nameWizard;
+
+
+
+
+        var random = new Random();
+
+
+
+
 
 
 
@@ -62,6 +117,21 @@ public class Program
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             try
             {
                 oP = Convert.ToInt32(Console.ReadLine());
@@ -76,57 +146,85 @@ public class Program
             }
 
 
+
+
+
+
+
+
             switch (oP)
             {
                 case 1:
                     totalWizard = 0;
 
 
+
+
+
+
+
+
                     Console.Write(GuardAsk);
-                    Console.WriteLine(EspecificationName);
                     try
                     {
                         nameWizard = Console.ReadLine()!;
+
+
+
+
+
+
 
 
                     }
                     catch (NullReferenceException)
                     {
                         Console.WriteLine(GuardRage);
-                        Console.WriteLine(EspecificationName+"!!");
                     }
                     catch (Exception)
                     {
                         Console.WriteLine(GuardRage);
-                        Console.WriteLine(EspecificationName + "!!");
                     }
+
+
 
 
                     nameWizard = Console.ReadLine()!;
 
 
+
+
                     string upperName = nameWizard.ToUpper();
+
+
 
 
                     string firtsLetter = upperName.Remove(1);
 
 
+
+
                     string nameCreato = nameWizard.Substring(1);
+
+
 
 
                     string realName = firtsLetter.Insert(1, nameCreato);
 
 
+
+
+
+
                     Console.WriteLine(GuardLore);
                     Console.WriteLine("Your mind: I will meditate for that time.");
-                    
+
 
                     for (int i = 1; i <= 5; i++)
                     {
                         Console.WriteLine("==============================");
 
 
-                        var random = new Random();
 
 
 
@@ -134,10 +232,16 @@ public class Program
                         int trainingTime = random.Next(1, 25);
 
 
+
+
                         powerWizard = random.Next(1, 11);
 
 
+
+
                         totalWizard += powerWizard;
+
+
 
 
 
@@ -147,64 +251,263 @@ public class Program
 
 
 
+
+
                         if (totalWizard < 20)
                         {
-                            Console.WriteLine($"Title:{WizardTitle1}, You repeat in the 2nd call.");
-                
+                            Console.WriteLine(WizardTitle + WizardTitle1 + WizardText1);
+
+
                         }
-                        else if ( 20<= totalWizard && totalWizard <= 29)
+                        else if (20 <= totalWizard && totalWizard <= 29)
                         {
-                            Console.WriteLine($"Title:{WizardTitle2}, You still confuse the rod with a spoon.");
+                            Console.WriteLine(WizardTitle + WizardTitle2 + WizardText2);
                         }
-                        else if (30 <= totalWizard && totalWizard <=34)
+                        else if (30 <= totalWizard && totalWizard <= 34)
                         {
-                            Console.WriteLine($"Title:{WizardTitle3},You are a Summoner of Magic Breezes.");
+                            Console.WriteLine(WizardTitle + WizardTitle3 + WizardText3);
                         }
-                        else if (35 <= totalWizard && totalWizard <=39)
+                        else if (35 <= totalWizard && totalWizard <= 39)
                         {
-                            Console.WriteLine($"Title:{WizardTitle4},Wow! You can summon dragons without burning down the lab!");
+                            Console.WriteLine(WizardTitle + WizardTitle4 + WizardText4);
                         }
-                        else 
+                        else
                         {
-                            Console.WriteLine($"Title:{WizardTitle5},You have reached the rank of Master of Arcana!.");
+                            Console.WriteLine(WizardTitle + WizardTitle5 + WizardText5);
                         }
                     }
                     break;
 
+
+
+
                 case 2:
+                    string[] ArrayMonster = { "Wandering Skeleton 💀", "Forest Goblin 👹", "Green Slime", "Ember Wolf 🐺", "Giant Spider 🕷️", "Iron Golem 🤖", "Lost Necromancer 🧝‍♂️", "Ancient Dragon 🐉" };
+                    int[] ArrayHp = { 3, 5, 10, 11, 18, 15, 20, 50 };
+
+
+
+
+
+
+
+
+                    int randomEnemy = random.Next(0, 8);
+                    Console.WriteLine(EnemyEncounter + ArrayMonster[randomEnemy] + EnemyEncounterHp + ArrayHp[randomEnemy]);
+
+
+                    int enemyHp = ArrayHp[randomEnemy];
+
+
+
+                    int numberDice;
+                    do
+                    {
+
+
+
+
+                        numberDice = random.Next(1, 7);
+
+
+                        if (numberDice == 1)
+                        {
+                            Console.WriteLine(DiceOne);
+                            enemyHp -= 1;
+                        }
+                        else if (numberDice == 2)
+                        {
+                            Console.WriteLine(DiceTwo);
+                            enemyHp -= 2;
+                        }
+                        else if (numberDice == 3)
+                        {
+                            Console.WriteLine(DiceTree);
+                            enemyHp -= 3;
+                        }
+                        else if (numberDice == 4)
+                        {
+                            Console.WriteLine(DiceFour);
+                            enemyHp -= 4;
+                        }
+                        else if (numberDice == 5)
+                        {
+                            Console.WriteLine(DiceFive);
+                            enemyHp -= 5;
+                        }
+                        else
+                        {
+
+
+                            Console.WriteLine(DiceSix);
+                            enemyHp -= 6;
+                        }
+                        Console.WriteLine(ActionAttack);
+                        Console.ReadKey();
+                        Console.WriteLine(ArrayMonster[randomEnemy] + TotalHp + enemyHp);
+                    }
+                    while (enemyHp > 0);
+
+
+                    if (enemyHp <= 0)
+                    {
+
+
+                        WizardLvl += 1;
+                        Console.WriteLine(LvlUp + WizardLvl);
+                    }
+
 
                     break;
                 case 3:
 
-                    
-                 break;
+
+                    int[,] matMine = new int[5, 5];
+                    string[,] simbolMine = new string[5, 5];
+
+
+                    int obtainCoin = random.Next(1, 101);
+
+
+                    int coinValue = random.Next(5, 51);
+
+
+
+
+                    for (int x = 0; x < 5; x++)
+                    {
+
+
+                        for (int y = 0; y < 5; y++)
+                        {
+                            simbolMine[x, y] = "➖";
+                        }
+                    }
+
+
+                    for (int x = 0; x < 5; x++)
+                    {
+                        coinValue = random.Next(5, 51);
+                        obtainCoin = random.Next(1, 101);
+
+
+                        for (int y = 0; y < 5; y++)
+                        {
+                            if (obtainCoin <= 50)
+                            {
+                                matMine[x, y] = 0;
+                            }
+                            else
+                            {
+                                matMine[x, y] = coinValue;
+                            }
+
+                        }
+                    }
+
+
+
+
+
+
+                    int minerTrys = 1;
+
+
+                    while (minerTrys <= 5)
+                    {
+                        Console.WriteLine(MineX);
+                        int x = int.Parse(Console.ReadLine());
+
+
+                        Console.WriteLine(MineY);
+                        int y = int.Parse(Console.ReadLine());
+
+
+                        if (matMine[x, y] == 0)
+                        {
+                            simbolMine[x, y] = "❌";
+                        }
+                        else
+                        {
+                            simbolMine[x, y] = " 🪙 ";
+
+
+                        }
+                        minerTrys++;
+                        Console.WriteLine("You obtained " + matMine[x, y] + "Bitcoins");
+                        totalBitcoins += matMine[x, y];
+                        Console.WriteLine("Your total bitcoins are" + totalBitcoins);
+                        for (int i = 0; i < 5; i++)
+                        {
+
+
+
+
+                            for (int j = 0; j < 5; j++)
+                            {
+                                Console.Write(simbolMine[i, j] + "  ");
+                            }
+                            Console.WriteLine();
+                        }
+
+
+
+
+
+
+
+
+                    }
+
+
+
+
+
+
+
+
+                    break;
 
 
                 case 4:
 
 
+
+
+
+
+
+
                 case 5:
 
 
-                case 6: 
-                
-                
-                
+
+
+
+
+
+
+                case 6:
+
+
+
+
+
+
                 case 7:
 
 
+
+
+
+
+
+
                 default:
-
-
                     Console.WriteLine(Error);
-
-
                     break;
             }
-
-
-
-
         } while (oP != 8);
     }
 }
+
