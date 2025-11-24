c8818929c31a0cce8c41eac7fd55d4e7ded1cf83
izan-gonzalez <Izan.gonzalez.7e8@itb.cat>
2025-11-24 16:53:59 +0100

    same fix from cha4

--- Program.cs
+++ Program.cs
@@ -453,18 +453,14 @@ public class Program
                 case 5:
                     string[] AddToInventory(string[] inventory, string item)
                     {
-                        // Crear nuevo array 1 espacio más grande
                         string[] newInventory = new string[arrayInventory.Length + 1];
 
 
-                        // Copiar todo lo anterior
                         for (int i = 0; i < inventory.Length; i++)
                         {
                             newInventory[i] = inventory[i];
                         }
 
-
-                        // Añadir el nuevo objeto
                         newInventory[newInventory.Length - 1] = item;
 
 
